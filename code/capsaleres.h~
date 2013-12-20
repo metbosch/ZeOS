c1d8be482f870cd859778ce09a3df974b1bc6184
Jaume <boschpons@gmail.com>
2013-12-20 15:38:22 +0100

    Result of first day

---
+++ code/capsaleres.h~
@@ -0,0 +1,5 @@
+void clock_handler();
+void keyboard_handler();
+void system_call_handler();
+
+long long int zeos_ticks;
---
+++ code/sys.c
@@ -0,0 +1,63 @@
+/*
+ * sys.c - Syscalls implementation
+ */
+#include <devices.h>
+
+#include <utils.h>
+
+#include <io.h>
+
+#include <mm.h>
+
+#include <mm_address.h>
+
+#include <sched.h>
+
+#define LECTURA 0
+#define ESCRIPTURA 1
+
+int check_fd(int fd, int permissions)
+{
+  if (fd!=1) return -9; /*EBADF*/
+  if (permissions!=ESCRIPTURA) return -13; /*EACCES*/
+  return 0;
+}
+
+int sys_ni_syscall()
+{
+	return -38; /*ENOSYS*/
+}
+
+int sys_getpid()
+{
+	return current()->PID;
+}
+
+int sys_fork()
+{
+  int PID=-1;
+
+  // creates the child process
+  
+  return PID;
+}
+
+void sys_exit()
+{  
+}
+
+
+int sys_write(int fd, char * buffer, int size) {
+/*    fd: file descriptor. In this delivery it must always be 1.
+      buffer: pointer to the bytes.
+      size: number of bytes.
+      return ’ Negative number in case of error (specifying the kind of error) and
+      the number of bytes written if OK.*/
+      if(check_fd(fd,ESCRIPTURA) != 0) return check_fd(fd,ESCRIPTURA);
+      if (buffer == NULL) return -1;
+      if (size < 0) return size;
+      for (int i = 0; i < size; ++i) {
+            printx_xy
+      }
+
+}
